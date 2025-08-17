55df4dab270808c4c36395ce6935c73ff7665bcb
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2025-08-17 16:31:13 +0530

    Added the QuestionRepository interface

---
+++ QuoraReactiveApp/src/main/java/com/example/QuoraReactiveApp/Repository/QuestionRepository.java
@@ -0,0 +1,9 @@
+package com.example.QuoraReactiveApp.Repository;
+
+import com.example.QuoraReactiveApp.Model.Question;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends ReactiveMongoRepository<Question,String> {
+}

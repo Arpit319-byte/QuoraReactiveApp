9bc2f42100433919a8996410d6a53059635505f4
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2025-08-17 15:58:49 +0530

    Added the attributes for the Question and BaseModel

--- QuoraReactiveApp/src/main/java/com/example/QuoraReactiveApp/Model/BaseModel.java
+++ QuoraReactiveApp/src/main/java/com/example/QuoraReactiveApp/Model/BaseModel.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.util.Date;
 
@@ -13,10 +16,15 @@ import java.util.Date;
 @NoArgsConstructor
 public class BaseModel {
 
+    @Id
     private String id;
 
+    @CreatedDate
     private Date createdAt;
 
+    @LastModifiedDate
     private Date updatedAt;
+
+
 
 }
--- QuoraReactiveApp/src/main/java/com/example/QuoraReactiveApp/Model/Question.java
+++ QuoraReactiveApp/src/main/java/com/example/QuoraReactiveApp/Model/Question.java
@@ -1,8 +1,28 @@
 package com.example.QuoraReactiveApp.Model;
 import com.example.QuoraReactiveApp.Model.BaseModel;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "questions")
 public class Question extends BaseModel{
 
+
+    @NotBlank(message = "Title cannot be blank")
     private String title;
+
+    @NotBlank(message = "Content cannot be blank")
+    private String content;
 
 }

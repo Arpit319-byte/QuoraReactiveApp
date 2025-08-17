24ba8e05a812434bcc9579eae51fb4cc31dbca87
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2025-08-17 15:34:27 +0530

    Added the attributes for the QuestionResponseDTO

---
+++ QuoraReactiveApp/src/main/java/com/example/QuoraReactiveApp/DTO/QuestionResponseDTO.java
@@ -0,0 +1,21 @@
+package com.example.QuoraReactiveApp.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionResponseDTO{
+
+    private String id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+}
